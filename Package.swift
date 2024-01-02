7ecbfa0d6f44710d3f3b88f537274b1ed4f7175e
Bruce <homan311@gmail.com>
2024-01-02 04:30:50 -0500

    Add chekcsum

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://document123.us-east-1.linodeobjects.com/WebRTC.xcframework.zip"
+            url: "https://document123.us-east-1.linodeobjects.com/WebRTC.xcframework.zip",
+            checksum: "b427d69e6b08dd865b1b3895b24e0a3f74d191124b22ce0468d4360f374c3f13"
         ),
     ]
 )
