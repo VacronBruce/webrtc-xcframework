0f6044445ae04050dba1c89a3fc830d76abe51fd
Bruce <homan311@gmail.com>
2024-01-02 04:17:27 -0500

    Change to customization webrtc

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/livekit/webrtc-xcframework-static/releases/download/114.5735.10/WebRTC.xcframework.zip",
-            checksum: "f767f4c8574e80b297bae7894ef7285e3e308db0fbf28bd5cf507c2d7e8e114e"
+            url: "https://document123.us-east-1.linodeobjects.com/WebRTC.xcframework.zip"
         ),
     ]
 )
